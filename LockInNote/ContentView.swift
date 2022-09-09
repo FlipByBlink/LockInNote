82346d69df08083b8e6b17d75229a8e084876559
aaaaa <aaaaa>
2022-09-09 19:08:11 +0900

    Initial Commit

---
+++ LockInNote/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LockInNote
+//
+//  Created by Mei Sei on 2022/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LockInNote/LockInNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInNoteApp.swift
+//  LockInNote
+//
+//  Created by Mei Sei on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct LockInNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
