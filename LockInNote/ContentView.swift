ec3c2c95a14ab56fbee994e0856f2d46599340ae
aaaaa <aaaaa>
2023-01-30 21:09:52 +0900

    アプリから離脱した際に広告シートを自動で閉じるように変更

--- LockInNote/ContentView.swift
+++ LockInNote/ContentView.swift
@@ -749,11 +749,17 @@ struct 🔗URLSchemeActionButton: View {
 
 struct 📣ADContent: ViewModifier {
     @EnvironmentObject var 🛒: 🛒StoreModel
+    @Environment(\.scenePhase) var scenePhase: ScenePhase
     @State private var ⓐpp: 📣MyApp = .pickUpAppWithout(.LockInNote)
     func body(content: Content) -> some View {
         content
             .sheet(isPresented: $🛒.🚩showADSheet) {
                 📣ADSheet(self.ⓐpp)
+            }
+            .onChange(of: self.scenePhase) {
+                if $0 == .background {
+                    🛒.🚩showADSheet = false
+                }
             }
     }
 }
