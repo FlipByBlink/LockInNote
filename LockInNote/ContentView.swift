d08647560a4da34a4d460a08a95dbf7e01ef8340
aaaaa <aaaaa>
2023-03-25 11:04:28 +0900

    🔗URLSchemeAction 🛠MenuTab

--- LockInNote/ContentView.swift
+++ LockInNote/ContentView.swift
@@ -16,9 +16,9 @@ struct ContentView: View {
             📝InlineWidgetTab()
                 .tag(🔖Tab.inlineWidget)
                 .tabItem { Label("Inline", systemImage: "rectangle.and.pencil.and.ellipsis") }
-            🛠OptionTab()
-                .tag(🔖Tab.option)
-                .tabItem { Label("Option", systemImage: "gearshape") }
+            🛠MenuTab()
+                .tag(🔖Tab.menu)
+                .tabItem { Label("Menu", systemImage: "gearshape") }
         }
         .scrollDismissesKeyboard(.interactively)
         .onOpenURL { ⓤrl in
@@ -35,7 +35,7 @@ struct ContentView: View {
         .modifier(📣ADContent())
     }
     private enum 🔖Tab {
-        case rectangularWidget, circularWidget, inlineWidget, option
+        case rectangularWidget, circularWidget, inlineWidget, menu
     }
 }
 
@@ -562,166 +562,18 @@ struct 🄾ptionIcon: View {
     }
 }
 
-struct 🛠OptionTab: View { // ⚙️
+struct 🛠MenuTab: View { // ⚙️
     var body: some View {
         NavigationStack {
             List {
                 ℹ️AboutAppSection()
                 📣ADMenuLink()
-                self.🔗URLSchemeActionSection()
+                🔗URLSchemeActionMenuLink()
                 //Section { // reject を受けて一旦 comment out
                 //    Text("If lock screen widgets don't update, please close this app or switch to another app.")
                 //} header: { Text("Directions") }
             }
             .navigationTitle("Menu")
         }
-    }
-    private func 🔗URLSchemeActionSection() -> some View {
-        Section {
-            NavigationLink {
-                🔗URLSchemeActionSettingMenu()
-            } label: {
-                Label("Setting URL scheme action", systemImage: "command")
-            }
-            HStack {
-                Spacer()
-                VStack {
-                    Text("shortcuts://run-shortcut?nam...")
-                        .rotationEffect(.degrees(-1.5))
-                    Text(verbatim: "https://duckduckgo.com/?q=...")
-                        .rotationEffect(.degrees(-1.5))
-                    Text("etc.")
-                        .rotationEffect(.degrees(-1.5))
-                }
-                Spacer()
-            }
-            .font(.subheadline.weight(.heavy).italic())
-            .lineLimit(1)
-            .foregroundStyle(.secondary)
-            .scaleEffect(y: 1.33)
-            .padding(.vertical, 32)
-        }
-    }
-    private struct 🔗URLSchemeActionSettingMenu: View {
-        @AppStorage("URLSchemeLeading") var 🔗leading: String = ""
-        @AppStorage("URLSchemeTrailing") var 🔗trailing: String = ""
-        @AppStorage("URLSchemeButtonTitle") var 🪧buttonTitle: String = ""
-        @AppStorage("EraseTextAfterAction") var 🚩eraseTextAfterAction: Bool = false
-        private var ⓛeading: String { self.🔗leading.isEmpty ? "① + " : self.🔗leading }
-        private var ⓣrailing: String { self.🔗trailing.isEmpty ? " + ②" : self.🔗trailing }
-        var body: some View {
-            List {
-                Section {
-                    VStack {
-                        HStack {
-                            if self.🔗leading.isEmpty {
-                                Text("① +")
-                                    .foregroundStyle(.secondary)
-                            }
-                            Text(self.🔗leading + "TEXT" + self.🔗trailing)
-                                .italic()
-                                .font(.system(.subheadline, design: .monospaced))
-                                .multilineTextAlignment(.center)
-                                .padding(8)
-                                .frame(minHeight: 100)
-                            if self.🔗trailing.isEmpty {
-                                Text("+ ②")
-                                    .foregroundStyle(.secondary)
-                            }
-                        }
-                        TextField("① URL scheme", text: self.$🔗leading)
-                        TextField("② Trailing component", text: self.$🔗trailing)
-                            .font(.subheadline)
-                            .padding(.bottom, 4)
-                    }
-                    .textFieldStyle(.roundedBorder)
-                } header: {
-                    Text("URL scheme")
-                }
-                Section {
-                    TextField("Input text of button", text: self.$🪧buttonTitle)
-                        .textFieldStyle(.roundedBorder)
-                } header: {
-                    Text("Button label")
-                }
-                Section {
-                    Toggle(isOn: self.$🚩eraseTextAfterAction) {
-                        Label("Erase text after action", systemImage: "eraser.line.dashed")
-                    }
-                }
-                Section {
-                    Text("shortcuts://run-shortcut?name=SHORTCUTTITLE&input=text&text=TEXT")
-                        .font(.system(.subheadline, design: .monospaced, weight: .medium))
-                        .multilineTextAlignment(.center)
-                        .padding(.vertical)
-                        .foregroundStyle(.secondary)
-                } header: {
-                    Text("Example 1")
-                } footer: {
-                    Text("To run one shortcut of Shortcuts App with the text.")
-                }
-                Section {
-                    HStack {
-                        Spacer()
-                        Text(verbatim: "https://duckduckgo.com/?q=TEXT")
-                            .font(.system(.subheadline, design: .monospaced, weight: .medium))
-                            .multilineTextAlignment(.center)
-                            .padding(.vertical)
-                            .foregroundStyle(.secondary)
-                        Spacer()
-                    }
-                } header: {
-                    Text("Example 2")
-                } footer: {
-                    Text("To search the text on DuckDuckGo.")
-                }
-            }
-            .navigationTitle("Customize Action")
-        }
-    }
-}
-
-struct 🔗URLSchemeActionButton: View {
-    @AppStorage("URLSchemeLeading") var 🔗leading: String = ""
-    @AppStorage("URLSchemeTrailing") var 🔗trailing: String = ""
-    @AppStorage("URLSchemeButtonTitle") var 🪧buttonTitle: String = ""
-    @AppStorage("EraseTextAfterAction") var 🚩eraseTextAfterAction: Bool = false
-    @Environment(\.openURL) var ⓞpenURL: OpenURLAction
-    @Binding var ⓠuery: String
-    private var ⓤrl: URL? {
-        let ⓣext = self.🔗leading + self.ⓠuery + self.🔗trailing
-        if let ⓔncodedText = ⓣext.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
-            return URL(string: ⓔncodedText)
-        } else {
-            return nil
-        }
-    }
-    var body: some View {
-        if !self.🔗leading.isEmpty {
-            if let ⓤrl {
-                Section {
-                    Button {
-                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                        self.ⓞpenURL.callAsFunction(ⓤrl) { ⓐccepted in
-                            if ⓐccepted && self.🚩eraseTextAfterAction {
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                    self.ⓠuery = ""
-                                }
-                            }
-                        }
-                    } label: {
-                        Label(LocalizedStringKey(self.🪧buttonTitle.isEmpty ? "URL Scheme Action" : self.🪧buttonTitle),
-                              systemImage: "command")
-                            .font(.headline)
-                    }
-                    .disabled(self.ⓠuery.isEmpty)
-                } footer: {
-                    Text(ⓤrl.description)
-                }
-            }
-        }
-    }
-    init(_ query: Binding<String>) {
-        self._ⓠuery = query
     }
 }
---
+++ LockInNote/🧩Sub/🔗URLSchemeAction.swift
@@ -0,0 +1,153 @@
+import SwiftUI
+
+struct 🔗URLSchemeActionButton: View {
+    @AppStorage("URLSchemeLeading") var 🔗leading: String = ""
+    @AppStorage("URLSchemeTrailing") var 🔗trailing: String = ""
+    @AppStorage("URLSchemeButtonTitle") var 🪧buttonTitle: String = ""
+    @AppStorage("EraseTextAfterAction") var 🚩eraseTextAfterAction: Bool = false
+    @Environment(\.openURL) var ⓞpenURL: OpenURLAction
+    @Binding var ⓠuery: String
+    private var ⓤrl: URL? {
+        let ⓣext = self.🔗leading + self.ⓠuery + self.🔗trailing
+        if let ⓔncodedText = ⓣext.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+            return URL(string: ⓔncodedText)
+        } else {
+            return nil
+        }
+    }
+    var body: some View {
+        if !self.🔗leading.isEmpty {
+            if let ⓤrl {
+                Section {
+                    Button {
+                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                        self.ⓞpenURL.callAsFunction(ⓤrl) { ⓐccepted in
+                            if ⓐccepted && self.🚩eraseTextAfterAction {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                    self.ⓠuery = ""
+                                }
+                            }
+                        }
+                    } label: {
+                        Label(LocalizedStringKey(self.🪧buttonTitle.isEmpty ? "URL Scheme Action" : self.🪧buttonTitle),
+                              systemImage: "command")
+                        .font(.headline)
+                    }
+                    .disabled(self.ⓠuery.isEmpty)
+                } footer: {
+                    Text(ⓤrl.description)
+                }
+            }
+        }
+    }
+    init(_ query: Binding<String>) {
+        self._ⓠuery = query
+    }
+}
+
+struct 🔗URLSchemeActionMenuLink: View {
+    var body: some View {
+        Section {
+            NavigationLink {
+                🔗URLSchemeActionMenu()
+            } label: {
+                Label("Setting URL scheme action", systemImage: "command")
+            }
+            HStack {
+                Spacer()
+                VStack {
+                    Text("shortcuts://run-shortcut?nam...")
+                        .rotationEffect(.degrees(-1.5))
+                    Text(verbatim: "https://duckduckgo.com/?q=...")
+                        .rotationEffect(.degrees(-1.5))
+                    Text("etc.")
+                        .rotationEffect(.degrees(-1.5))
+                }
+                Spacer()
+            }
+            .font(.subheadline.weight(.heavy).italic())
+            .lineLimit(1)
+            .foregroundStyle(.secondary)
+            .scaleEffect(y: 1.33)
+            .padding(.vertical, 32)
+        }
+    }
+}
+
+private struct 🔗URLSchemeActionMenu: View {
+    @AppStorage("URLSchemeLeading") var 🔗leading: String = ""
+    @AppStorage("URLSchemeTrailing") var 🔗trailing: String = ""
+    @AppStorage("URLSchemeButtonTitle") var 🪧buttonTitle: String = ""
+    @AppStorage("EraseTextAfterAction") var 🚩eraseTextAfterAction: Bool = false
+    private var ⓛeading: String { self.🔗leading.isEmpty ? "① + " : self.🔗leading }
+    private var ⓣrailing: String { self.🔗trailing.isEmpty ? " + ②" : self.🔗trailing }
+    var body: some View {
+        List {
+            Section {
+                VStack {
+                    HStack {
+                        if self.🔗leading.isEmpty {
+                            Text("① +")
+                                .foregroundStyle(.secondary)
+                        }
+                        Text(self.🔗leading + "TEXT" + self.🔗trailing)
+                            .italic()
+                            .font(.system(.subheadline, design: .monospaced))
+                            .multilineTextAlignment(.center)
+                            .padding(8)
+                            .frame(minHeight: 100)
+                        if self.🔗trailing.isEmpty {
+                            Text("+ ②")
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                    TextField("① URL scheme", text: self.$🔗leading)
+                    TextField("② Trailing component", text: self.$🔗trailing)
+                        .font(.subheadline)
+                        .padding(.bottom, 4)
+                }
+                .textFieldStyle(.roundedBorder)
+            } header: {
+                Text("URL scheme")
+            }
+            Section {
+                TextField("Input text of button", text: self.$🪧buttonTitle)
+                    .textFieldStyle(.roundedBorder)
+            } header: {
+                Text("Button label")
+            }
+            Section {
+                Toggle(isOn: self.$🚩eraseTextAfterAction) {
+                    Label("Erase text after action", systemImage: "eraser.line.dashed")
+                }
+            }
+            Section {
+                Text("shortcuts://run-shortcut?name=SHORTCUTTITLE&input=text&text=TEXT")
+                    .font(.system(.subheadline, design: .monospaced, weight: .medium))
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical)
+                    .foregroundStyle(.secondary)
+            } header: {
+                Text("Example 1")
+            } footer: {
+                Text("To run one shortcut of Shortcuts App with the text.")
+            }
+            Section {
+                HStack {
+                    Spacer()
+                    Text(verbatim: "https://duckduckgo.com/?q=TEXT")
+                        .font(.system(.subheadline, design: .monospaced, weight: .medium))
+                        .multilineTextAlignment(.center)
+                        .padding(.vertical)
+                        .foregroundStyle(.secondary)
+                    Spacer()
+                }
+            } header: {
+                Text("Example 2")
+            } footer: {
+                Text("To search the text on DuckDuckGo.")
+            }
+        }
+        .navigationTitle("Customize Action")
+    }
+    }
