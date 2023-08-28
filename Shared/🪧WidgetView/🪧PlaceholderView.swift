162163f35abf663d23cc949a62075b7fcbeece1d
aaaaa <aaaaa>
2023-08-28 19:46:56 +0900

    a

--- Shared/🪧WidgetView/🪧PlaceholderView.swift
+++ Shared/🪧WidgetView/🪧PlaceholderView.swift
@@ -1,16 +1,31 @@
 import SwiftUI
 
 struct 🪧PlaceholderView : View {
+    @Environment(\.widgetFamily) var widgetFamily
     var body: some View {
         VStack(alignment: .leading) {
-            Text(verbatim: "AAAAAAAAAAAA")
-            Text(verbatim: "AAAAAAAAAAAA")
-            Text(verbatim: "AAAAAAAAAAAA")
-            Text(verbatim: "AAAAAAA")
+            switch self.widgetFamily {
+                case .systemSmall, .systemMedium, .systemLarge, .systemExtraLarge:
+                    Group {
+                        Text(verbatim: "AAAAAAAAA")
+                        Text(verbatim: "AAAAAAAAA")
+                        Text(verbatim: "AAAAAAAAA")
+                        Text(verbatim: "AAAAA")
+                    }
+                    .font(.title3)
+                case .accessoryCircular, .accessoryRectangular:
+                    Text(verbatim: "AAAAA")
+                    Text(verbatim: "AAAAA")
+                    Text(verbatim: "AAA")
+                default:
+                    Text(verbatim: "AAAAA")
+                    Text(verbatim: "AAAAA")
+                    Text(verbatim: "AAA")
+            }
         }
-        .font(.title3)
+        .minimumScaleFactor(0.4)
+        .lineLimit(1)
         .redacted(reason: .placeholder)
-        .minimumScaleFactor(0.6)
         .modifier(🪧ContainerBackground())
     }
 }
