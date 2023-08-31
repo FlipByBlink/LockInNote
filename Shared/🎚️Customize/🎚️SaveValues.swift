9210c2e3b86f47a87699d2c9afc41c9f0533a6fc
aaaaa <aaaaa>
2023-08-31 16:01:34 +0900

    Self(ⓒases: self.ⓒases.dropLast())

--- Shared/🎚️Customize/🎚️SaveValues.swift
+++ Shared/🎚️Customize/🎚️SaveValues.swift
@@ -2,34 +2,45 @@ import SwiftUI
 
 struct 🎚️SaveValues: ViewModifier {
     @EnvironmentObject var note: 📝NoteModel
+    private var ⓒases: [📝NoteProperty] = .Element.allCases
     func body(content: Content) -> some View {
-        content
-            .onChange(of: self.note.fontWeight) { self.note.save(.fontWeight, $0) }
-            .onChange(of: self.note.fontDesign) { self.note.save(.fontDesign, $0) }
-            .onChange(of: self.note.italic) { self.note.save(.italic, $0) }
-            .onChange(of: self.note.multilineTextAlignment) { self.note.save(.multilineTextAlignment, $0) }
-        
-        //MARK: Empty icon
-            .onChange(of: self.note.empty_type) { self.note.save(.empty_type, $0) }
-            .onChange(of: self.note.empty_iconSize) { self.note.save(.empty_iconSize, $0) }
-            .onChange(of: self.note.empty_userText) { self.note.save(.empty_userText, $0) }
-        
-        //MARK: WidgetFamily.systemSmall, .systemMedium, .systemLarge, .systemExtraLarge
-            .onChange(of: self.note.system_appearanceMode) { self.note.save(.system_appearanceMode, $0) }
-            .onChange(of: self.note.system_fontSize) { self.note.save(.system_fontSize, $0) }
-            .onChange(of: self.note.system_hierarchical) { self.note.save(.system_hierarchical, $0) }
-            .onChange(of: self.note.system_padding) { self.note.save(.system_padding, $0) }
-            .onChange(of: self.note.system_contentAlignment) { self.note.save(.system_contentAlignment, $0) }
-            .onChange(of: self.note.system_textColor) { self.note.save(.system_textColor, 🎚️Color($0)) }
-            .onChange(of: self.note.system_backgroundColor) { self.note.save(.system_backgroundColor, 🎚️Color($0)) }
-            .onChange(of: self.note.system_backgroundGradient) { self.note.save(.system_backgroundGradient, $0) }
-            .onChange(of: self.note.system_doubleSizeOnLargeWidget) { self.note.save(.system_doubleSizeOnLargeWidget, $0) }
-        
-        //MARK: WidgetFamily.accessoryInline, .accessoryCircular, .accessoryRectangular
-            .onChange(of: self.note.accessory_fontSize) { self.note.save(.accessory_fontSize, $0) }
-            .onChange(of: self.note.accessory_hierarchical) { self.note.save(.accessory_hierarchical, $0) }
-            .onChange(of: self.note.accessoryCircular_backgroundForIOS16AndWatchOS) {
-                self.note.save(.accessoryCircular_backgroundForIOS16AndWatchOS, $0)
+        if let ⓒase = ⓒases.last {
+            content
+                .modifier(Self.SaveValue(property: ⓒase))
+                .modifier(Self(ⓒases: self.ⓒases.dropLast()))
+        } else {
+            content
+        }
+    }
+    private struct SaveValue: ViewModifier {
+        @EnvironmentObject var note: 📝NoteModel
+        var property: 📝NoteProperty
+        func body(content: Content) -> some View {
+            switch self.property {
+                case .text, .title: content
+                case .fontWeight: content.onChange(of: self.note.fontWeight) { self.save($0) }
+                case .fontDesign: content.onChange(of:self.note.fontDesign) { self.save($0) }
+                case .italic: content.onChange(of:self.note.italic) { self.save($0) }
+                case .multilineTextAlignment: content.onChange(of:self.note.multilineTextAlignment) { self.save($0) }
+                case .empty_type: content.onChange(of:self.note.empty_type) { self.save($0) }
+                case .empty_iconSize: content.onChange(of:self.note.empty_iconSize) { self.save($0) }
+                case .empty_userText: content.onChange(of:self.note.empty_userText) { self.save($0) }
+                case .system_appearanceMode: content.onChange(of:self.note.system_appearanceMode) { self.save($0) }
+                case .system_fontSize: content.onChange(of:self.note.system_fontSize) { self.save($0) }
+                case .system_hierarchical: content.onChange(of:self.note.system_hierarchical) { self.save($0) }
+                case .system_padding: content.onChange(of:self.note.system_padding) { self.save($0) }
+                case .system_contentAlignment: content.onChange(of:self.note.system_contentAlignment) { self.save($0) }
+                case .system_textColor: content.onChange(of:self.note.system_textColor) { self.save(🎚️Color($0)) }
+                case .system_backgroundColor: content.onChange(of:self.note.system_backgroundColor) { self.save(🎚️Color($0)) }
+                case .system_backgroundGradient: content.onChange(of:self.note.system_backgroundGradient) { self.save($0) }
+                case .system_doubleSizeOnLargeWidget: content.onChange(of:self.note.system_doubleSizeOnLargeWidget) { self.save($0) }
+                case .accessory_fontSize: content.onChange(of:self.note.accessory_fontSize) { self.save($0) }
+                case .accessory_hierarchical: content.onChange(of:self.note.accessory_hierarchical) { self.save($0) }
+                case .accessoryCircular_backgroundForIOS16AndWatchOS: content.onChange(of:self.note.accessoryCircular_backgroundForIOS16AndWatchOS) { self.save($0) }
             }
+        }
+        private func save(_ ⓥalue: some Codable) {
+            self.note.save(self.property, ⓥalue)
+        }
     }
 }
